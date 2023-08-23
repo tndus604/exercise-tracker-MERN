013e01cfa4e764dbe7502d7c66321a0d19a88a6a
tndus604 <tndus0604@hotmail.com>
2023-08-23 18:24:34 -0400

    app.js in backend created

---
+++ exercise-rest/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+// middleware
+const corsOptions = {
+    origin: "https://exercise-tracker-mern-gn98.onrender.com" // frontend URI (ReactJS)
+}
+app.use(express.json());
+app.use(cors(corsOptions));
+
+// connect MongoDB
+mongoose.connect(process.env.MONGODB_CONNECT_STRING).then(() => {
+    const PORT = process.env.PORT || 8000
+    app.listen(PORT, () => {
+        console.log(`App is Listening on PORT ${PORT}`);
+    })
+}).catch(err => {
+    console.log(err);
+});
+
+// route
+app.get("/", (req, res) => {
+    res.status(201).json({message: "Connected to Backend!"});
+});
